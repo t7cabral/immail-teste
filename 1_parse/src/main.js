dd1f3661b87acdb3d7848e7669fffcecb6eb2578
Thiago Cabral <thiago231286@gmail.com>
2019-09-12 16:57:02 -0300

    ajustes no codigo

--- 1_parse/src/main.js
+++ 1_parse/src/main.js
@@ -22,21 +22,25 @@ const fs = require('fs')
 const readline = require('readline')
 const mongoose = require('mongoose')
 
+try {
+   // conexão com MongoDB
+   mongoose.connect('mongodb+srv://thiago:immail123@cluster0-gsysb.mongodb.net/immail?retryWrites=true&w=majority',
+      { useNewUrlParser: true,  useUnifiedTopology: true }
+   );
+} catch (err) {
+   console.log(err)
+   process.exit(1)
+}
 
-mongoose.connect('mongodb+srv://thiago:immail123@cluster0-gsysb.mongodb.net/immail?retryWrites=true&w=majority',
-   { useNewUrlParser: true,  useUnifiedTopology: true }
-);
 
-
-
-
-
-
+// lendo arquivo game.log
 let rl = readline.createInterface({
    input: fs.createReadStream('./src/games.log', {encoding: 'utf8'})
 });
 
-let xx = {}
+
+// Variáveis
+let game = {}
 let gameNo = 0
 let gameName = ''
 let gameKill = 0
@@ -44,16 +48,15 @@ let gamePlayers = []
 let gamePlayerKill = []
 let gameOpen = false
 
-// event is emitted after each line
+
+// evento emitido após cada linha
 rl.on('line', function(line) {
 
-
-   // verifica inicio de partida e se partida atual esta aberta
    if( line.indexOf("InitGame:") !== -1 && gameOpen == true ) {
-      /* Verdadeiro quando é encontrada uma partida nova com uma em aberto  */
+      /* entra quando encontra a flag de nova partida "InitGame:" e existe uma partida aberta "gameOpen == true"  */
 
       // salva partida atual
-      xx[gameName] = {
+      game[gameName] = {
             'total_kills': gameKill,
             'players': gamePlayers,
             'kills': gamePlayerKill
@@ -66,8 +69,9 @@ rl.on('line', function(line) {
       gamePlayers = []
       gamePlayerKill = []
       gameOpen = true
+
    } else if(  line.indexOf("InitGame:") !== -1 && gameOpen == false) {
-      /* Verdadeiro quando não há partidas abertas */
+      /* entra quando encontra a flag de nova partida "InitGame:" e NÃO existe partida aberta "gameOpen == false"  */
 
       // cria nova partida
       gameNo++ // incrementa a variável
@@ -76,22 +80,24 @@ rl.on('line', function(line) {
       gamePlayers = []
       gamePlayerKill = []
       gameOpen = true
+
    } else if( line.indexOf("ShutdownGame:") !== -1 ) {
-      /* Verdadeiro quando encontra o fechamendo da partida. */
+      /* entra quando encontra a flag de fechamendo da partida "ShutdownGame:" */
 
       // salva partida atual
-      xx[gameName] = {
+      game[gameName] = {
          'total_kills': gameKill,
          'players': gamePlayers,
          'kills': gamePlayerKill
       }
 
-      // sinaliza que a partida foi fechada e limpa os kill da mesma
-      gameOpen = false
+      // sinaliza que a partida foi fechada e limpa as variáveis
+      gameKill = 0
       gamePlayers = []
       gamePlayerKill = []
-      gameKill = 0
+      gameOpen = false
    }
+
    
    // get players
    if( line.indexOf('ClientUserinfoChanged:') !== -1 ) {
@@ -101,9 +107,10 @@ rl.on('line', function(line) {
       */
       player = line.split("\\")[1].trim()
 
-      // verifica se jogador está na lista antes de adicionar
+      // adiciona o jogador na lista de jogadores
       if (gamePlayers.includes(player) === false) gamePlayers.push(player);
 
+      // adiciona o jogador no contador de kill atribuindo o valor 0 de início
       if( !gamePlayerKill.hasOwnProperty(player) ) {
          gamePlayerKill[player] = 0
       }
@@ -113,62 +120,79 @@ rl.on('line', function(line) {
    // contagem dos Kill
    if( line.indexOf("Kill:") !== -1 ) {
 
-      // contador geral de kill
+      // contador geral de kill "todas as mortes são contabilizadas aqui, inclusive mortes pelo <world>"
       gameKill++
 
+      // contador específico de kill "por jogador"
+      sepKilled = line.split('killed') // divido a string pela palavra "killed"
+      author = sepKilled[0].split(':')[3].trim() // divido pelo caractere ":" e pego o autor (quem matou)
+      victim = sepKilled[1].split('by')[0].trim() // divido pelo caractere "by" e pego a vitima (quem morreu)
 
-      // contador específico de kill
-      sepKilled = line.split('killed')
-      author = sepKilled[0].split(':')[3].trim()
-      victim = sepKilled[1].split('by')[0].trim()
-
+      // adiciona o autor (quem matou) na lista de kills se ele for diferente de <world>.
+      // atenção: <world> não entra nessa contagem individuais de kills
       if( !gamePlayerKill.hasOwnProperty(author) && author !== '<world>' ) {
-         gamePlayerKill[author] = 0
+         gamePlayerKill[author] = 0 // inicializa o jogador com valor 0
       }
 
+      // adiciona o jogador vítima (quem morreu) na lista de contagem de kills com valor inicial 0
       if( !gamePlayerKill.hasOwnProperty(victim) ) {
          gamePlayerKill[victim] = 0
       }
 
+      // adiciona 1 ponto para o autor (quem matou) caso ele seja diferente de <world>
       if( author !== '<world>' ) {
          gamePlayerKill[author] = gamePlayerKill[author] + 1
       }
 
+      // subtrai 1 ponto da vitima (quem morreu) se o autor (quem matou) for o <world>
       if( author === '<world>' ) {
          gamePlayerKill[victim] = gamePlayerKill[victim] - 1
       }
-
-      //process.exit(1)
 
    }
 
 });
 
 
-// end
+// evento emitido quando se completa a leitura do arquivo game.log
 rl.on('close', function(line) {
 
-   
+   // get conexão com MongoDB
    let con = mongoose.connection;
 
-   Object.entries(xx).forEach( (item, key) => {
+   // quantidade de partidas
+   const totalGame = Object.keys(game).length
+
+   // loop de partidas
+   Object.entries(game).forEach( (item, key) => {
 
       let objPlayerKill = Object.assign({}, item[1].kills)
 
-      con.collection('gameLog').updateOne(
-         { game_name: item[0] },
-         { $set: {
-               game_name: item[0],
-               total_kills: item[1].total_kills,
-               players: item[1].players,
-               kills: objPlayerKill
+      try {
+
+         // salva/atualiza a partida no banco de dados
+         con.collection('gameLog').updateOne(
+            { game_name: item[0] },
+            { $set: {
+                  game_name: item[0],
+                  total_kills: item[1].total_kills,
+                  players: item[1].players,
+                  kills: objPlayerKill
+               }
+            },
+            {
+               upsert: true
+            },
+            (err, doc) => {
+               // callback
+               console.log('Partida ',key+1,' de ',totalGame, ' salvo com sucesso!')
             }
-         },
-         { upsert:true }
-      )
+         )
+
+      } catch (err) {
+         console.error(err)
+      }
+
    })
    
 })
-
-
-
